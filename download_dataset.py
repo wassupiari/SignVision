dd69f745b27fdbced7f2a21893dac7d3d0c6cc33
iarinormanno <normannoiari@gmail.com>
2025-02-06 14:01:06 +0100

    Add: script per scarire il dataset

--- download_dataset.py
+++ download_dataset.py
@@ -1,20 +1,6 @@
-import os
-import zipfile
 import kagglehub
 
-# Percorso della cartella del progetto
-project_path = os.path.dirname(os.path.abspath(__file__))
+# Download latest version
+path = kagglehub.dataset_download("meowmeowmeowmeowmeow/gtsrb-german-traffic-sign")
 
-def download_and_extract_dataset(dataset_name, extract_path):
-    print(f"Downloading dataset {dataset_name}...")
-    dataset_path = kagglehub.dataset_download(dataset_name, path=extract_path)
-    print(f"Dataset scaricato in: {dataset_path}")
-
-# Nome del dataset su KaggleHub
-dataset_name = "meowmeowmeowmeowmeow/gtsrb-german-traffic-sign"
-
-# Percorso della cartella di destinazione
-extract_to_path = os.path.join(project_path, "GTSRB")
-
-# Scaricare ed estrarre il dataset
-download_and_extract_dataset(dataset_name, extract_to_path)
+print("Path to dataset files:", path)
