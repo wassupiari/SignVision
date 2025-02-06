a7a6f62f9e2e20c0e133c7d87aee63891e61d912
iarinormanno <normannoiari@gmail.com>
2025-02-06 14:21:55 +0100

    Fix: cambiati i path per il dataset

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from plot_metrics import plot_training_history
 import os
 
 # Configurazione
-train_data_path = "../MoodRipple"
-test_data_path = "../MoodRipple"
-test_labels_path = "Dataset/Test.csv"
-train_labels_path = "Dataset/Train.csv"
+dir = "../MoodRipple/gtsrb-german-traffic-sign"
+
+test_labels_path = "../MoodRipple/gtsrb-german-traffic-sign/Test.csv"
+train_labels_path = "../MoodRipple/gtsrb-german-traffic-sign/Train.csv"
 img_size = (30, 30)
 epochs = 15
 batch_size = 32
@@ -17,11 +17,11 @@ model_save_path = "models/traffic_signs_model_v3.h5"
 
 def main():
     # Caricamento e preprocessing del dataset
-    train_images, train_labels = load_data(train_labels_path, train_data_path, img_size)
+    train_images, train_labels = load_data(train_labels_path, dir, img_size)
     train_images, val_images, train_labels, val_labels = split_data(train_images, train_labels)
 
     # Caricamento e preprocessing del test set
-    test_images, test_labels = load_data(test_labels_path, test_data_path, img_size)
+    test_images, test_labels = load_data(test_labels_path, dir, img_size)
 
     # Creazione del modello
     model = create_model(input_shape=(img_size[0], img_size[1], 3), num_classes=train_labels.shape[1])
